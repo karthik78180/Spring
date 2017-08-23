a9e5c9973527d477ff16e94ca8c337ba1e90869b
karthik78180 <karthik78180@gmail.com>
2017-08-23 16:51:55 -0500

    Basic Spring app IoC 1.0.1

---
+++ spring-demo-one/src/com/luv2code/springdemo/BaseBallCoach.java
@@ -0,0 +1,9 @@
+package com.luv2code.springdemo;
+
+public class BaseBallCoach implements Coach {
+	@Override
+	public String getDailyWorkout() {
+		return "this is from baseball coach";
+	}
+
+}
---
+++ spring-demo-one/src/com/luv2code/springdemo/HelloSpringApp.java
@@ -0,0 +1,23 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+
+	public static void main(String[] args) {
+		//load the spring configuration file
+		
+		ClassPathXmlApplicationContext context =
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		//retrive bean from Spring container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		//call methods on th ebean
+		System.out.println(theCoach.getDailyWorkout());
+		
+		//close the context
+		context.close();
+
+	}
+
+}
